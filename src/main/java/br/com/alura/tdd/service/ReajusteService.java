0f3db06d561e6468570debb6c5d72e52cb1d933b
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-28 13:04:50 -0300

    Refactor salary adjustment service
    Changes
    - Apply strategy pattern by forcing each enum constant to implement a
      method to return it's adjustment percentage
    - Remove switch statement from adjustment service and replace with call
      enum constant method

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,28 +8,8 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        BigDecimal reajustePorcento;
-        BigDecimal reajuste;
-
-        switch (desempenho) {
-            case A_DESEJAR:
-                reajustePorcento = new BigDecimal("0.03");
-                break;
-
-            case BOM:
-                reajustePorcento = new BigDecimal("0.15");
-                break;
-
-            case OTIMO:
-                reajustePorcento = new BigDecimal("0.20");
-                break;
-
-            default:
-                reajustePorcento = BigDecimal.ZERO;
-                break;
-        }
-
-        reajuste = funcionario.getSalario().multiply(reajustePorcento);
+        BigDecimal percentualReajuste = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentualReajuste);
 
         funcionario.reajustarSalario(reajuste);
     }
